3a4bbbc849466505d2e80f29ccc2fc54c96a3766
pablocordonie <pablogarcia.93.06@gmail.com>
2024-05-01 19:13:22 +0200

    feat: reconfigure the data scrapper in index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const { default: puppeteer } = require('puppeteer');
 
+const instantGamingProducts = [];
+
 const gamingProductsSchema = new Schema({
     title: { type: String, required: true },
     img: { type: String },
-    price: { type: String },
+    price: { type: Number },
+    stock: { type: String }
 },
     {
         timestamps: true,
@@ -24,7 +27,56 @@ const connectDB = async () => {
     }
 };
 
+const dataCollector = async (page, browser) => {
+    let arrayDivs = await page.$$(".force-badge");
+
+    for (let gameDiv of arrayDivs) {
+        let title = await gameDiv.$eval(".title", (el) => el.innerText);
+        let img = await gameDiv.$eval("img", (el) => el.src);
+        let price;
+
+        try {
+            price = await gameDiv.$eval(".price", (el) => Number(el.innerText.slice(0, el.innerText.length - 1)));
+
+            const game = {
+                title,
+                img,
+                price
+            };
+            instantGamingProducts.push(game);
+        } catch (error) {
+            const game = {
+                title,
+                img,
+                stock: 'Not Available'
+            };
+            instantGamingProducts.push(game);
+        }
+    };
+
+    try {
+        await page.$eval("[title='Next']", (el) => el.click());
+        await page.waitForNavigation();
+        dataCollector(page, browser);
+    } catch (error) {
+        await browser.close();
+
+        mongoose.connect(process.env.DB_URL).then(async () => {
+            const gamesCollection = await Product.find();
+            if (gamesCollection.length) {
+                await Product.collection.drop();
+                console.log(`The games collection's been dropped`);
+            }
+        }).catch(error => console.log(`Error deleting the games collection: ${error}`)).then(async () => {
+            const gamingProductsData = instantGamingProducts.map(product => new Product(product));
+            await Product.insertMany(gamingProductsData);
+            console.log('All the gaming products have been successfully saved in the DB: ', instantGamingProducts);
+        }).catch(error => console.error('An error has happened while saving the gaming products to the database: ', error));
+    }
+};
+
 const gamingProductsScrapper = async (url) => {
+
     await connectDB();
 
     const browser = await puppeteer.launch({
@@ -39,32 +91,13 @@ const gamingProductsScrapper = async (url) => {
     await page.waitForSelector('div.actions .main button.button.button-secondary');
     await page.$eval('div.actions .main button.button.button-secondary:last-of-type', (el) => el.click());
 
-    await page.$eval('.games a.title-more', (el) => el.click());
+    await page.$eval('#nav-nintendo a.access.mimic', (el) => el.click());
+    await page.waitForSelector('#nav-nintendo-panel');
+    await page.$eval('#nav-nintendo-panel .platforms a', (el) => el.click());
 
     await page.waitForSelector('footer');
-    const title = await page.$$eval('div.top-sales.listing-items .item .title', (nodes) => nodes.map(node => node.innerText));
-    const img = await page.$$eval('div.top-sales.listing-items .item .picture', (nodes) => nodes.map(node => node.src));
-    const price = await page.$$eval('div.top-sales.listing-items .item .price', (nodes) => nodes.map(node => node.innerText));
 
-    const instantGamingProducts = title.map((value, index) => {
-        return {
-            title: title[index],
-            img: img[index],
-            price: price[index]
-        }
-    });
-
-    instantGamingProducts.map(async (product) => {
-        const productSchema = new Product(product);
-        try {
-            await productSchema.save();
-            console.log(`${productSchema.title} has been successfully saved in the DB`);
-        } catch (err) {
-            console.error(`${productSchema.title} couldn't be saved in the DB, error: `, err);
-        }
-    });
-
-    await browser.close();
+    dataCollector(page, browser);
 };
 
 gamingProductsScrapper('https://www.instant-gaming.com/es/');
